e2844e9c8248682e45fef92321a92df8113ccdc6
Hemant soni <sonihemant4741@gmail.com>
2020-05-13 20:20:09 +0530

    class File created for reading file

---
+++ lib/load_file.rb
@@ -0,0 +1,16 @@
+class File
+  attr_reader :file, :file_content
+
+  def intialize(file)
+    @file = file
+    @file_content = get_content(@file)
+  end
+
+  def get_content(file)
+    file_content = ''
+    File.open(file, 'r') { |strings| file_content = strings.readlines.map(&:chomp) }
+    scanned_content = file_content { |string| StringScanner.new(string) }
+
+    scanned_content
+  end
+end
